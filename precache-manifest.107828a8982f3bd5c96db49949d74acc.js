795f462e7c2cb19beceb0eac3fb9effd9ca07309
Paul <lpault29@gmail.com>
2018-11-11 18:11:39 -0500

    Updates

---
+++ precache-manifest.107828a8982f3bd5c96db49949d74acc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "be19e768a1431bf39569",
+    "url": "/mccanncc/static/js/runtime~main.be19e768.js"
+  },
+  {
+    "revision": "d469deda3a88e1c713d6",
+    "url": "/mccanncc/static/js/main.d469deda.chunk.js"
+  },
+  {
+    "revision": "a07fdf898aa1dd7fa42e",
+    "url": "/mccanncc/static/js/3.a07fdf89.chunk.js"
+  },
+  {
+    "revision": "7585265e32679e0ac4fa",
+    "url": "/mccanncc/static/js/2.7585265e.chunk.js"
+  },
+  {
+    "revision": "aa95c47c2e03cea5829f",
+    "url": "/mccanncc/static/js/1.aa95c47c.chunk.js"
+  },
+  {
+    "revision": "d469deda3a88e1c713d6",
+    "url": "/mccanncc/static/css/main.023cc980.chunk.css"
+  },
+  {
+    "revision": "98b71021a4266fc8712d168e86d2e510",
+    "url": "/mccanncc/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/mccanncc/precache-manifest.b1d61f35aa526b8b5f6336ae85792082.js"
+  "/mccanncc/precache-manifest.107828a8982f3bd5c96db49949d74acc.js"
 );
 
 workbox.clientsClaim();
